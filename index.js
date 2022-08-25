a6c35121a0e8ba332a1f924cfe580cd3c5d28d39
olerinskaia <olerinskaia.work@gmail.com>
2022-08-25 23:07:21 +0300

    feat: add difficulty

--- index.js
+++ index.js
@@ -3,25 +3,13 @@ import brownCardsData from './data/mythicCards/brown/index.js';
 import greenCardsData from './data/mythicCards/green/index.js';
 import ancientsData from './data/ancients.js';
 
-//выбор древнего
-const containerWithAncients = document.querySelector('.ancient-container');
-// добавление класса ancient-card-active к выбранному древнему
-let selectedAncient;
-containerWithAncients.onclick = () => {
-  let target = event.target;
-  if (target.className !== 'ancient-card') return;
-  highlight(target);
-};
-function highlight(img) {
-  if (selectedAncient) {
-    selectedAncient.classList.remove('ancient-card-active');
-  }
-  selectedAncient = img;
-  selectedAncient.classList.add('ancient-card-active');
-}
-
 //выбор сложности
 const difficultyContainer = document.querySelector('.difficulty-container');
+const superEasy = document.querySelector('#superEasy');
+const easy = document.querySelector('#easy');
+const medium = document.querySelector('#medium');
+const high = document.querySelector('#high');
+const hard = document.querySelector('#hard');
 // добавление класса difficulty-button-active к выбранной кнопки со сложностью
 let selectedDifficultyButton;
 difficultyContainer.onclick = () => {
@@ -37,12 +25,16 @@ function highlightDifficulty(div) {
   selectedDifficultyButton.classList.add('difficulty-button-active');
 }
 
-//тасуем средний уровень сложности
 const deck = document.querySelector('.deck');
 const lastCard = document.querySelector('.last-card');
 const stageText1 = document.querySelector('.stage-text1');
 const stageText2 = document.querySelector('.stage-text2');
 const stageText3 = document.querySelector('.stage-text3');
+
+const azathoth = document.querySelector('#azathoth');
+const cthulthu = document.querySelector('#cthulthu');
+const iogSothoth = document.querySelector('#iogSothoth');
+const shubNiggurath = document.querySelector('#shubNiggurath');
 
 const reshuffleGreen = reshuffleCards(greenCardsData);
 const reshuffleBrown = reshuffleCards(brownCardsData);
@@ -58,12 +50,88 @@ function reshuffleCards(arr) {
   return result;
 }
 
-//средняя сложность
+//средняя сложность Азатот
 
-const firstStageDeck = [];
-const secondStageDeck = [];
-const thirdStageDeck = [];
+let firstStageDeck = [];
+let secondStageDeck = [];
+let thirdStageDeck = [];
 
+const superEasyDifficulty = (countCards, emptyArr) => {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  while (i < countCards.greenCards) {
+    const randomNumber = Math.floor(Math.random() * reshuffleGreen.length);
+    if (reshuffleGreen[randomNumber].difficulty === 'hard') {
+      continue;
+    } else if (reshuffleGreen[randomNumber].difficulty === 'normal') {
+      continue;
+    }
+    emptyArr.push(reshuffleGreen[randomNumber]);
+    i++;
+    if (i < countCards.greenCards) {
+      if (reshuffleGreen[randomNumber].difficulty === 'hard') {
+        continue;
+      } else if (reshuffleGreen[randomNumber].difficulty === 'easy') {
+        continue;
+      }
+      emptyArr.push(reshuffleGreen[randomNumber]);
+    }
+  }
+  while (j < countCards.brownCards) {
+    const randomNumber = Math.floor(Math.random() * reshuffleBrown.length);
+    if (reshuffleBrown[randomNumber].difficulty === 'hard') {
+      continue;
+    } else if (reshuffleBrown[randomNumber].difficulty === 'normal') {
+      continue;
+    }
+    emptyArr.push(reshuffleBrown[randomNumber]);
+    j++;
+  }
+  while (k < countCards.blueCards) {
+    const randomNumber = Math.floor(Math.random() * reshuffleBlue.length);
+    if (reshuffleBlue[randomNumber].difficulty === 'hard') {
+      continue;
+    } else if (reshuffleBlue[randomNumber].difficulty === 'normal') {
+      continue;
+    }
+    emptyArr.push(reshuffleBlue[randomNumber]);
+    k++;
+  }
+
+  return reshuffleCards(emptyArr);
+};
+const easyDifficulty = (countCards, emptyArr) => {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  while (i < countCards.greenCards) {
+    const randomNumber = Math.floor(Math.random() * reshuffleGreen.length);
+    if (reshuffleGreen[randomNumber].difficulty === 'hard') {
+      continue;
+    }
+    emptyArr.push(reshuffleGreen[randomNumber]);
+    i++;
+  }
+  while (j < countCards.brownCards) {
+    const randomNumber = Math.floor(Math.random() * reshuffleBrown.length);
+    if (reshuffleBrown[randomNumber].difficulty === 'hard') {
+      continue;
+    }
+    emptyArr.push(reshuffleBrown[randomNumber]);
+    j++;
+  }
+  while (k < countCards.blueCards) {
+    const randomNumber = Math.floor(Math.random() * reshuffleBlue.length);
+    if (reshuffleBlue[randomNumber].difficulty === 'hard') {
+      continue;
+    }
+    emptyArr.push(reshuffleBlue[randomNumber]);
+    k++;
+  }
+
+  return reshuffleCards(emptyArr);
+};
 const mediumDifficulty = (countCards, emptyArr) => {
   let i = 0;
   let j = 0;
@@ -86,39 +154,311 @@ const mediumDifficulty = (countCards, emptyArr) => {
 
   return reshuffleCards(emptyArr);
 };
+const highDifficulty = (countCards, emptyArr) => {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  while (i < countCards.greenCards) {
+    const randomNumber = Math.floor(Math.random() * reshuffleGreen.length);
+    if (reshuffleGreen[randomNumber].difficulty === 'easy') {
+      continue;
+    }
+    emptyArr.push(reshuffleGreen[randomNumber]);
+    i++;
+  }
+  while (j < countCards.brownCards) {
+    const randomNumber = Math.floor(Math.random() * reshuffleBrown.length);
+    if (reshuffleBrown[randomNumber].difficulty === 'easy') {
+      continue;
+    }
+    emptyArr.push(reshuffleBrown[randomNumber]);
+    j++;
+  }
+  while (k < countCards.blueCards) {
+    const randomNumber = Math.floor(Math.random() * reshuffleBlue.length);
+    if (reshuffleBlue[randomNumber].difficulty === 'easy') {
+      continue;
+    }
+    emptyArr.push(reshuffleBlue[randomNumber]);
+    k++;
+  }
 
-const a = mediumDifficulty(ancientsData[0].firstStage, firstStageDeck);
-const b = mediumDifficulty(ancientsData[0].secondStage, secondStageDeck);
-const c = mediumDifficulty(ancientsData[0].thirdStage, thirdStageDeck);
+  return reshuffleCards(emptyArr);
+};
+const hardDifficulty = (countCards, emptyArr) => {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  while (i < countCards.greenCards) {
+    const randomNumber = Math.floor(Math.random() * reshuffleGreen.length);
+    if (reshuffleGreen[randomNumber].difficulty === 'easy') {
+      continue;
+    } else if (reshuffleGreen[randomNumber].difficulty === 'normal') {
+      continue;
+    }
+    emptyArr.push(reshuffleGreen[randomNumber]);
+    i++;
+  }
+  while (j < countCards.brownCards) {
+    const randomNumber = Math.floor(Math.random() * reshuffleBrown.length);
+    if (reshuffleBrown[randomNumber].difficulty === 'easy') {
+      continue;
+    } else if (reshuffleBrown[randomNumber].difficulty === 'normal') {
+      continue;
+    }
+    emptyArr.push(reshuffleBrown[randomNumber]);
+    j++;
+  }
+  while (k < countCards.blueCards) {
+    const randomNumber = Math.floor(Math.random() * reshuffleBlue.length);
+    if (reshuffleBlue[randomNumber].difficulty === 'easy') {
+      continue;
+    } else if (reshuffleBlue[randomNumber].difficulty === 'normal') {
+      continue;
+    }
+    emptyArr.push(reshuffleBlue[randomNumber]);
+    k++;
+  }
 
-const middle = [...a, ...b, ...c];
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(middle);
-let count = 0;
-deck.onclick = () => {
-  const showCard = (arr) => {
-    if (count < arr.length) {
-      lastCard.style.backgroundImage = `url('${arr[count].cardFace}')`;
-      console.log(`url('${arr[count].cardFace}')`);
-    }
-    if (count == arr.length) {
-      deck.style.backgroundImage = 'none';
-      stageText3.style.color = 'black';
-    }
-    if (count == 0) {
-      stageText1.style.color = 'red';
-    }
-    if (count == a.length) {
-      stageText1.style.color = 'black';
-      stageText2.style.color = 'red';
-    }
-    if (count == a.length + b.length) {
-      stageText2.style.color = 'black';
-      stageText3.style.color = 'red';
-    }
-    count++;
+  return reshuffleCards(emptyArr);
+};
+const choiceDifficultyLvl = (numAncient) => {
+  if (superEasy.classList.contains('difficulty-button-active') == true) {
+    const a = superEasyDifficulty(
+      ancientsData[numAncient].firstStage,
+      firstStageDeck
+    );
+    const b = superEasyDifficulty(
+      ancientsData[numAncient].secondStage,
+      secondStageDeck
+    );
+    const c = superEasyDifficulty(
+      ancientsData[numAncient].thirdStage,
+      thirdStageDeck
+    );
+    const middle = [...a, ...b, ...c];
+    return middle;
+  }
+  if (easy.classList.contains('difficulty-button-active') == true) {
+    const a = easyDifficulty(
+      ancientsData[numAncient].firstStage,
+      firstStageDeck
+    );
+    const b = easyDifficulty(
+      ancientsData[numAncient].secondStage,
+      secondStageDeck
+    );
+    const c = easyDifficulty(
+      ancientsData[numAncient].thirdStage,
+      thirdStageDeck
+    );
+    const middle = [...a, ...b, ...c];
+    return middle;
+  }
+  if (medium.classList.contains('difficulty-button-active') == true) {
+    const a = mediumDifficulty(
+      ancientsData[numAncient].firstStage,
+      firstStageDeck
+    );
+    const b = mediumDifficulty(
+      ancientsData[numAncient].secondStage,
+      secondStageDeck
+    );
+    const c = mediumDifficulty(
+      ancientsData[numAncient].thirdStage,
+      thirdStageDeck
+    );
+    const middle = [...a, ...b, ...c];
+
+    return middle;
+  }
+  if (high.classList.contains('difficulty-button-active') == true) {
+    const a = highDifficulty(
+      ancientsData[numAncient].firstStage,
+      firstStageDeck
+    );
+    const b = highDifficulty(
+      ancientsData[numAncient].secondStage,
+      secondStageDeck
+    );
+    const c = highDifficulty(
+      ancientsData[numAncient].thirdStage,
+      thirdStageDeck
+    );
+    const middle = [...a, ...b, ...c];
+    return middle;
+  }
+  if (hard.classList.contains('difficulty-button-active') == true) {
+    const a = hardDifficulty(
+      ancientsData[numAncient].firstStage,
+      firstStageDeck
+    );
+    const b = hardDifficulty(
+      ancientsData[numAncient].secondStage,
+      secondStageDeck
+    );
+    const c = hardDifficulty(
+      ancientsData[numAncient].thirdStage,
+      thirdStageDeck
+    );
+    const middle = [...a, ...b, ...c];
+    return middle;
+  }
+};
+
+azathoth.onclick = () => {
+  azathoth.classList.add('ancient-card-active');
+  cthulthu.classList.remove('ancient-card-active');
+  iogSothoth.classList.remove('ancient-card-active');
+  shubNiggurath.classList.remove('ancient-card-active');
+
+  deck.style.backgroundImage = `url('./assets/mythicCardBackground.png')`;
+  lastCard.style.backgroundImage = 'none';
+  const a = 4;
+  const b = 6;
+
+  let count = 0;
+  deck.onclick = () => {
+    const showCard = (arr) => {
+      if (count < arr.length) {
+        lastCard.style.backgroundImage = `url('${arr[count].cardFace}')`;
+        console.log(`url('${arr[count].cardFace}')`);
+      }
+      if (count === arr.length) {
+        deck.style.backgroundImage = 'none';
+        stageText3.style.color = 'black';
+      }
+      if (count === 0) {
+        stageText1.style.color = 'red';
+      }
+      if (count === a) {
+        stageText1.style.color = 'black';
+        stageText2.style.color = 'red';
+      }
+      if (count === a + b) {
+        stageText2.style.color = 'black';
+        stageText3.style.color = 'red';
+      }
+      count++;
+    };
+
+    showCard(choiceDifficultyLvl(0));
   };
-  showCard(middle);
+};
+
+cthulthu.onclick = () => {
+  azathoth.classList.remove('ancient-card-active');
+  cthulthu.classList.add('ancient-card-active');
+  iogSothoth.classList.remove('ancient-card-active');
+  shubNiggurath.classList.remove('ancient-card-active');
+  deck.style.backgroundImage = `url('./assets/mythicCardBackground.png')`;
+  lastCard.style.backgroundImage = 'none';
+  const a = 4;
+  const b = 4;
+
+  let count = 0;
+
+  deck.onclick = () => {
+    function showCard(arr) {
+      if (count < arr.length) {
+        lastCard.style.backgroundImage = `url('${arr[count].cardFace}')`;
+        console.log(`url('${arr[count].cardFace}')`);
+      }
+      if (count == arr.length) {
+        deck.style.backgroundImage = 'none';
+        stageText3.style.color = 'black';
+      }
+      if (count == 0) {
+        stageText1.style.color = 'red';
+      }
+      if (count == a) {
+        stageText1.style.color = 'black';
+        stageText2.style.color = 'red';
+      }
+      if (count == a + b) {
+        stageText2.style.color = 'black';
+        stageText3.style.color = 'red';
+      }
+      count++;
+    }
+
+    showCard(choiceDifficultyLvl(1));
+  };
+};
+iogSothoth.onclick = () => {
+  azathoth.classList.remove('ancient-card-active');
+  cthulthu.classList.remove('ancient-card-active');
+  iogSothoth.classList.add('ancient-card-active');
+  shubNiggurath.classList.remove('ancient-card-active');
+  deck.style.backgroundImage = `url('./assets/mythicCardBackground.png')`;
+  lastCard.style.backgroundImage = 'none';
+  const a = 3;
+  const b = 6;
+
+  let count = 0;
+
+  deck.onclick = () => {
+    function showCard(arr) {
+      if (count < arr.length) {
+        lastCard.style.backgroundImage = `url('${arr[count].cardFace}')`;
+        console.log(`url('${arr[count].cardFace}')`);
+      }
+      if (count == arr.length) {
+        deck.style.backgroundImage = 'none';
+        stageText3.style.color = 'black';
+      }
+      if (count == 0) {
+        stageText1.style.color = 'red';
+      }
+      if (count == a) {
+        stageText1.style.color = 'black';
+        stageText2.style.color = 'red';
+      }
+      if (count == a + b) {
+        stageText2.style.color = 'black';
+        stageText3.style.color = 'red';
+      }
+      count++;
+    }
+    showCard(choiceDifficultyLvl(2));
+  };
+};
+
+shubNiggurath.onclick = () => {
+  azathoth.classList.remove('ancient-card-active');
+  cthulthu.classList.remove('ancient-card-active');
+  iogSothoth.classList.remove('ancient-card-active');
+  shubNiggurath.classList.add('ancient-card-active');
+  deck.style.backgroundImage = `url('./assets/mythicCardBackground.png')`;
+  lastCard.style.backgroundImage = 'none';
+  const a = 4;
+  const b = 6;
+
+  let count = 0;
+  deck.onclick = () => {
+    function showCard(arr) {
+      if (count < arr.length) {
+        lastCard.style.backgroundImage = `url('${arr[count].cardFace}')`;
+        console.log(`url('${arr[count].cardFace}')`);
+      }
+      if (count == arr.length) {
+        deck.style.backgroundImage = 'none';
+        stageText3.style.color = 'black';
+      }
+      if (count == 0) {
+        stageText1.style.color = 'red';
+      }
+      if (count == a) {
+        stageText1.style.color = 'black';
+        stageText2.style.color = 'red';
+      }
+      if (count == a + b) {
+        stageText2.style.color = 'black';
+        stageText3.style.color = 'red';
+      }
+      count++;
+    }
+    console.log(choiceDifficultyLvl(3));
+    showCard(choiceDifficultyLvl(3));
+  };
 };
